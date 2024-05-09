11a870fe17629ff6213aee48c6abc79ff91dc612
daiki-kaneda <daiki0622developing@gmail.com>
2024-05-09 14:43:10 +0900

    added iap helper

---
+++ lib/helper/iap_helper.dart
@@ -0,0 +1,20 @@
+import 'package:in_app_purchase/in_app_purchase.dart';
+
+
+/*
+  IAPHelperの役割
+  - 初期化時にsave_data_helperのインスタンスを受け取って、shared_preferenceへアクセスできるようにする
+  - 初期化時にproductIdからproductDetailsのリストを取得して、変数に格納する
+     そして、InAppPurchaseのインスタンスのpurchaseStreamをリッスンして、購入、復元、エラー処理を実装した_handlePurchaseコールバックを渡す
+  - 購入時,購入復元時の処理ではsave_data_helperにおける適切な処理をする
+  - バッグエンドを使わないために、非消費型、サブスクリプション、記録の必要のない消費型のみを扱い、それらはenum型などでidとともに管理する
+  
+*/
+class IAPHelper{
+
+
+  Future<void> init()async{
+
+  }
+
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import cloud_firestore
 import connectivity_plus
 import device_info_plus
 import firebase_core
+import in_app_purchase_storekit
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

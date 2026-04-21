964c823a21dc63fe9eb1a29e6545c29b74f7ee53
daiki-kaneda <daiki0622developing@gmail.com>
2026-04-22 00:22:53 +0900

    feat: add color to problem timer

--- lib/screens/playing_page/stages/objects/problem/problem_timer.dart
+++ lib/screens/playing_page/stages/objects/problem/problem_timer.dart
@@ -1,11 +1,10 @@
-
 import 'dart:async';
 
 import 'package:bit_math/screens/playing_page/stages/objects/sentence.dart';
 import 'package:bit_math/utils/sprite_util.dart';
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
-
+import 'package:flutter/material.dart';
 
 // time
 // level1:15s
@@ -14,11 +13,9 @@ import 'package:flame/effects.dart';
 // level4:7s
 // level5:6s
 // level6:5s
-class ProblemTimer extends PositionComponent{
-  ProblemTimer({
-    required double x,
-    required double y,
-  }):gridPosition=Vector2(x, y);
+class ProblemTimer extends PositionComponent {
+  ProblemTimer({required double x, required double y})
+    : gridPosition = Vector2(x, y);
 
   final Vector2 gridPosition;
 
@@ -29,35 +26,47 @@ class ProblemTimer extends PositionComponent{
   @override
   FutureOr<void> onLoad() {
     sandglass = SpriteComponent(
-      sprite: getSprite(SpriteSheets.coloredTransparentPacked, 41*16, 12*16, 16, 16),
-      position: Vector2(gridPosition.x*16, gridPosition.y*16)
+      sprite: getSprite(
+        SpriteSheets.coloredTransparentPacked,
+        41 * 16,
+        12 * 16,
+        16,
+        16,
+      ),
+      position: Vector2(gridPosition.x * 16, gridPosition.y * 16),
     );
 
-    timelimit = 
-    Sentence(
-      gridPosition.x+1, gridPosition.y, 
-      str: time.toString().padLeft(2,'0'),
-      direction: SentenceDirection.horizontal);
+    timelimit = _getTimeLimit();
 
     countdown = EffectController(
-    infinite: true,
-    duration: 1.0,
-    onMax: () {
-      if(time>=1){
-        time-=1;
-        remove(timelimit);
-        timelimit = 
-        Sentence(
-          gridPosition.x+1, gridPosition.y, 
-          str: time.toString().padLeft(2,'0'),
-          direction: SentenceDirection.horizontal);
-        add(timelimit);
-      }
-    },
-  );
-   sandglass.add(ScaleEffect.to(Vector2.all(1.0), countdown));
+      infinite: true,
+      duration: 1.0,
+      onMax: () {
+        if (time >= 1) {
+          time -= 1;
+          remove(timelimit);
+          timelimit = _getTimeLimit();
+          add(timelimit);
+        }
+      },
+    );
+    sandglass.add(ScaleEffect.to(Vector2.all(1.0), countdown));
 
-   addAll([sandglass,timelimit]);
+    addAll([sandglass, timelimit]);
   }
-  
+
+  Sentence _getTimeLimit() {
+    final color = time == 0
+        ? Colors.red
+        : time <= 3
+        ? Colors.yellow
+        : null;
+    return Sentence(
+      gridPosition.x + 1,
+      gridPosition.y,
+      str: time.toString().padLeft(2, '0'),
+      direction: SentenceDirection.horizontal,
+      color: color,
+    );
+  }
 }
